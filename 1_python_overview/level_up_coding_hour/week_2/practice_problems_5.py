353f0811908db7231ec1cb0c5a10886d6bec8542
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-01-18 08:24:05 -0600

    Initial check-in

--- 1_python_overview/level_up_coding_hour/week_2/practice_problems_5.py
+++ 1_python_overview/level_up_coding_hour/week_2/practice_problems_5.py
@@ -4,15 +4,16 @@ Using a while loop, print all even numbers within the range 20 to 80, inclusive 
 '''
 
 # INITIALIZE PROGRAM
+range_start = 20
+range_stop = 81
+number = range_start
 
 # RUN PROGRAM
-# Loop through the number range and print only the even numbers
-for number in range(20, 81):
-    if number % 2 == 0:
+# Print only the even numbers in a given range using a WHILE loop
+while number in range(range_start, range_stop):
         print(number)
-    else:
-        continue
-        
+        number += 2 # Increment number by 2 so it skips ODD numbers and remains EVEN
+
 # END PROGRAM
 # Say good-bye
 print("Good-bye!")
