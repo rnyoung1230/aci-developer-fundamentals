457509f32f805e66be265972bd6e9733efcbe99d
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-01-19 10:18:22 -0600

    Initial check-in

---
+++ 2_data_structures_iterables/scratchpad.py
@@ -0,0 +1,35 @@
+# Basic list example
+# house_pets = ["dog", "cat", "hamster", "bird"]
+#
+# for pet in house_pets:
+#     print(pet)
+
+# # Slicing examples
+# my_list = ["first", "second", "third", "fourth"]
+#
+# # print single element using the positive index
+# print(my_list[1])
+#
+# # print single element using the negative index
+# print(my_list[-1])
+#
+# # print range of values using the slice operator
+# print(my_list[1:3])
+
+# Dictionary examples
+inventory = {
+    "apples": 5,
+    "bananas": 4,
+    "coconuts": 2
+}
+
+print (inventory)
+
+inv_with_dups = {
+    "apples": 5,
+    "bananas": 4,
+    "coconuts": 2,
+    "apples": 1
+}
+
+print (inv_with_dups)
