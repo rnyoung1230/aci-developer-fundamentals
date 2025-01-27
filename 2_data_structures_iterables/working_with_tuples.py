f49ee617892671480c389f96f94196c47cbed11a
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-01-27 08:02:50 -0600

    Initial commit

---
+++ 2_data_structures_iterables/working_with_tuples.py
@@ -0,0 +1,16 @@
+# e-learning code
+
+# Unpacking tuples
+"""
+Each transaction record includes:
+item name (str)
+quantity (int)
+date (str)
+price (float)
+"""
+purchase = ("product-1", 2, "6-1-2023", 5.99)
+
+item = purchase[0]
+quantity = purchase[1]
+print(item)
+print(quantity)
