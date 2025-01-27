31d4d5bb6c7a8d73b097e8f10eaffb53fbf48fc7
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-01-27 14:49:09 -0600

    Initial commit

--- 2_data_structures_iterables/access-nested-items.py
+++ 2_data_structures_iterables/access-nested-items.py
@@ -3,13 +3,17 @@
 # Step 1: Create a list of dictionaries to store employee details.
 # Each dictionary should have keys: "name", "age", "position", "department"
 employees = [
-    # YOUR CODE HERE
+    {"name": "Sally", "age": 25, "position": "Engineer", "department": "Sales"},
+    {"name": "John", "age": 53, "position": "Manager", "department": "IT"},
+    {"name": "Beth", "age": 37, "position": "Talent Recruiter", "department": "HR"}
 ]
 
 # Step 2: Use a loop to print each employee's name and department.
 # Example output: "John is in the Sales department."
-# YOUR CODE HERE
+for employee in employees:
+    print(f"{employee["name"]} is in the {employee["department"]} department.")
 
 # Step 3: Modify the loop to also print each employee's position.
 # Example output: "John is in the Sales department and works as a Manager."
-# YOUR CODE HERE
+for employee in employees:
+    print(f"{employee["name"]} is in the {employee["department"]} department and works as a {employee["position"]}.")
