de1c60955541e0aa98dc6aa984bcea117c955d76
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-01 08:12:21 -0600

    Initial commit

--- 2_data_structures_iterables/comprehensions_example.py
+++ 2_data_structures_iterables/comprehensions_example.py
@@ -1,3 +1,5 @@
+########      List Comprehensions example       #####
+
 list1 = [1,2,3,4,5,6]
 #we want to create a new list that has squared values from list1
 squared = []
@@ -7,6 +9,8 @@ squared = []
 #using a for loop to create the new list
 for n in list1:
     squared.append(n**2)
+
+print(squared)
 
 #using a list comprehension to create the new list
 square_list = [n**2 for n in list1]
@@ -22,9 +26,10 @@ three_times = []
 #using a loop
 for each_age in ages:
     three_times.append(3*each_age)
+print(three_times)
 
 #using list comprehension    
-triple_ages = [i**3 for i in ages]
+triple_ages = [i*3 for i in ages]
 print(triple_ages)
 
 ########      Dictionary Comprehensions example       #####
@@ -40,7 +45,15 @@ for i in range(len(numbers)):
 print(doubles)
 
 #using dictionary comprehension
-double_values = {num: num**2 for num in numbers}
+double_values = {num: num*2 for num in numbers}
 
 print(double_values)
+
+# dictionary comprehension using zip
+cities = ["Madison", "Chicago", "Los Angeles", "Atlanta", "New York"]
+states = ["WI", "IL", "CA", "GA", "NY"]
+
+new_dict = {cities: states for cities, states in zip(cities, states)}
+
+print(new_dict)
 
