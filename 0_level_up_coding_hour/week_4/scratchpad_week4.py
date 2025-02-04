fcccd9bbcc70de19467cbb7da244d65290752f8b
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-04 06:22:46 -0600

    Initial commit

--- 0_level_up_coding_hour/week_4/scratchpad_week4.py
+++ 0_level_up_coding_hour/week_4/scratchpad_week4.py
@@ -1,3 +1,12 @@
-print("Level Up Coding Hour: Week 4")
+# Week 4 Assessment Question
+sizes_avail = {'S', 'M', 'L', 'XL', 'XXL'}
+sizes_needed = {'XS', 'S', 'M', 'L', 'XL', 'XXL'}
 
-print("Hello World!")
+
+print(f"Answer 1: {sizes_needed.issubset(sizes_avail)}")
+
+print(f"Answer 2: {len(sizes_needed - sizes_avail) >= 0}")
+
+print(f"Answer 3: {sizes_needed in sizes_avail}")
+
+print(f"Answer 4: {sizes_needed.union(sizes_avail)}")
