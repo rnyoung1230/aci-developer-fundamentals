876500889a2e317c6c7e0c90ca4bc714507f4368
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-08 13:22:18 -0600

    Initial commit

---
+++ 3_functions/scratchpad_e-learning.py
@@ -0,0 +1,19 @@
+# WEEK 5 - FUNCTIONS
+
+# What are functions?
+# def my_function():
+#
+#     """Print a welcome message for users visiting the website."""
+#
+#     print("Welcome to our website!")
+#
+# my_function()
+
+# Math functions
+numbers = [2, 5, 9, 8, 3, 20]
+
+min_val = min(numbers)
+max_val = max(numbers)
+
+print("Minimum value is:", min_val)
+print("Maximum value is:", max_val)
