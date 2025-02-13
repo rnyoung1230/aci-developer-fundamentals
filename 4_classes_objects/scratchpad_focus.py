7b2e7f7f308348b97bd89c39548f61da776b1238
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-13 17:27:31 -0600

    Initial commit

---
+++ 4_classes_objects/scratchpad_focus.py
@@ -0,0 +1,4 @@
+# FOCUS SESSION - 2/12
+# ------------------------
+
+# CLASSES AND OBJECTS
---
+++ 4_classes_objects/scratchpad_spotlight.py
@@ -0,0 +1,59 @@
+# SPOTLIGHT SESSION - 2/11
+# ------------------------
+
+# Functions Review
+# def find_max(*nums):
+#     return max(nums)
+#
+# print(find_max(90, 1, 2, 3, 4, 10))
+
+####################################
+# WRITING CLEAN CODE - JASON'S TIPS
+####################################
+
+'''
+Docstring describing program
+'''
+
+# imports first
+
+# global and constant variables second
+
+# class definitions third
+
+# functions fourth
+
+# main thread of execution (the "main" part of your code)
+
+#####################################
+# EXAMPLE
+#####################################
+
+"""
+This module does X and Y.
+It provides functionality for Z.
+"""
+
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+from external_module import module_function
+
+CONSTANT_VALUE = 42
+
+class MyClass:
+    def __init__(self):
+        pass
+
+    def my_method(self):
+        pass
+
+def my_function():
+    pass
+
+if __name__ == "__main__":
+    # Main execution code here
+    print("Hello, World!")
