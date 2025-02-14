f6e7dfb038ddecc9cf6725cdc737453fcf47bcc1
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-14 11:26:59 -0600

    Initial commit

---
+++ 4_classes_objects/scratchpad_labology_0213_jasons_games.py
@@ -0,0 +1,32 @@
+# LABOLOGY SESSION - 2/13
+# ------------------------
+
+# Jason's Dice Game exercise
+'''
+Directions:
+
+'''
+import random
+
+class Dice:
+    # class variables and methods
+
+    # instance variables and methods
+    def __init__(self, value=None): # sometimes it's a good idea to have a default value assigned to your constructor args.
+        if value is None:
+            self.roll()
+        else:
+            self.value = value
+
+    def __str__(self):
+        return str(self.value)  # overriding this method allows you to then print(object) and not receive the memory location
+
+    def roll(self):
+        self.value = random.randint(1,6)
+
+if __name__ == '__main__':
+    dice = [Dice(), Dice(), Dice(), Dice(), Dice()]
+
+    for die in dice:
+        print(die)
+
