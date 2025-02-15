7976df0fc2a5188823e5828a9d5a40fd9772c9b3
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-15 09:50:57 -0600

    Initial commit

--- 0_level_up_coding_hour/week_6/coding_hour_0214.py
+++ 0_level_up_coding_hour/week_6/coding_hour_0214.py
@@ -5,27 +5,42 @@ xxxxxxxx
 '''
 import random
 
-class BankAccount:
-    # unique accounts
-    created_accounts = []
+# created bank accounts
+created_accounts = {"1000000": {
+    "type": "Savings",
+    "balance": 0
+}
+}
 
-    # instance constructor
-    def __init__(self):
+class BankAccount:
+    # class variables and methods
+
+    # instance variables and methods
+    def __init__(self, account_type="Savings"):
         self.account_number = self.assign_account_number()
-        while self.account_number in BankAccount.created_accounts:
+        while self.account_number in created_accounts:
             self.account_number = self.assign_account_number()
 
-        BankAccount.created_accounts.append(self.account_number)
+        self.account_type = account_type
         self.account_balance = 0
 
+        created_accounts[self.account_number] = {"type": self.account_type, "balance": self.account_balance}
+
+    def __str__(self):
+        print(f"Bank Account Summary\n"
+              f"Account Number: {self.account_number}\n"
+              f"Type: {self.account_type}")
+
     def assign_account_number(self):
-        return random.randrange(1000000, 9999999)
+        return str(random.randrange(1000001, 9999999))
 
 ################################## TEST BANK ACCOUNT CREATION ##################################
-for i in range(50):
-    my_account = BankAccount()
-    print(my_account.account_number, my_account.account_balance)
+for i in range(10):
+    new_checking_account = BankAccount("Checking")
 
-print(BankAccount.created_accounts)
-created_accounts_set = set(BankAccount.created_accounts)
-print(len(created_accounts_set), len(BankAccount.created_accounts))
+for i in range(10):
+    new_savings_account = BankAccount()
+
+print(created_accounts)
+created_accounts_set = set(created_accounts)
+print(len(created_accounts_set), len(created_accounts))
