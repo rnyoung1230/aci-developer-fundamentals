5232e0a0c0e1b1adc94c91aedb1f3526b0dcdaf1
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-16 06:53:16 -0600

    Various updates

--- 0_level_up_coding_hour/week_6/banking_system.py
+++ 0_level_up_coding_hour/week_6/banking_system.py
@@ -10,26 +10,31 @@ class Currency:
 
 class Bank:
     name = "ABC Bank"
-    active_accounts = []
+    accounts = {
+        "1000000": {
+            "status" : "active",
+        }
+    }
 
     @staticmethod
     def assign_account_number():
         number = random.randint(1000001, 9999999)
         # If number already in use, find another
-        while number in Bank.active_accounts:
+        while number in Bank.accounts:
             number = random.randint(1000001, 9999999)
         return number
 
     @staticmethod
     def open_account(account_type, opening_balance):
         account_num = Bank.assign_account_number()
-        Bank.active_accounts.append(account_num)
+        Bank.accounts[account_num] = {"status" : "active"}
+
         new_account = BankAccount(account_num, account_type, opening_balance)
         return new_account
 
     @staticmethod
     def close_account(account_num):
-        Bank.active_accounts.pop(account_num)
+        Bank.accounts[account_num]["status"] = "closed"
 
 class BankAccount:
     # class variables and methods
@@ -42,12 +47,20 @@ class BankAccount:
         self.account_balance = opening_balance
         self.transaction_history = []
 
+        new_transaction = BankTransaction("Open Account", opening_balance)
+        self.record_transaction(new_transaction)
+
     def __str__(self):
+        transaction_history_display = ""
+        for record in self.transaction_history:
+            transaction_history_display += ' '.join(f'{k}: {v}' for k, v in record.items()) + '\n'
+
         return f"BANK ACCOUNT SUMMARY\n" \
                f"Account Number: {self.account_number}\n" \
                f"Type: {self.account_type}\n" \
                f"Balance: {Currency.format_currency(self.account_balance)}\n" \
-               f"Transaction History: {self.transaction_history}\n" \
+               f"\nTRANSACTION HISTORY\n" \
+               f"{transaction_history_display}\n" \
                f"------------------------------------------"
 
     def make_deposit(self, amount):
@@ -63,14 +76,14 @@ class BankAccount:
         else:
             print(f"Insufficient funds. Withdrawal amount ({Currency.format_currency(amount)}) will cause your "
                   f"account balance ({Currency.format_currency(self.account_balance)}) "
-                  f"to fall below the required minimum ({Currency.format_currency(BankAccount.minimum_balance)})")
+                  f"to fall below the required minimum ({Currency.format_currency(BankAccount.minimum_balance)})\n")
 
     def record_transaction(self, transaction):
         transaction_record = {
-            "transaction_id": transaction.transaction_id,
-            "type": transaction.transaction_type,
-            "amount": transaction.transaction_amount,
-            "date": transaction.transaction_date
+            "Transaction ID": transaction.transaction_id,
+            "Date": transaction.transaction_date,
+            "Amount": transaction.transaction_amount,
+            "Type": transaction.transaction_type
         }
 
         self.transaction_history.append(transaction_record)
@@ -97,6 +110,7 @@ class BankTransaction:
                f"Date: {self.transaction_date}\n"
 
 ################################## TEST BANKING SYSTEM ##################################
+# Create a bunch of Checking and Savings accounts...execute deposit and withdrawal activities on each and then print a summary
 for i in range(10):
     new_checking_account = Bank.open_account(account_type="Checking", opening_balance=250)
     new_checking_account.make_deposit(random.randint(500, 1000))
@@ -109,8 +123,19 @@ for i in range(10):
     new_savings_account.make_withdrawal(random.randint(100, 1000))
     print(new_savings_account)
 
-# Confirm assign_account_number is working...list and set lengths should match
-accounts_set = set(Bank.active_accounts)
-print(f"Set length: {len(accounts_set)}, List length: {len(Bank.active_accounts)}")
-print(Bank.active_accounts)
+# Confirm assign_account_number is working (no duplicates)...list and set lengths should match
+accounts_set = set(Bank.accounts)
+print(f"Set length: {len(accounts_set)}, List length: {len(Bank.accounts)}")
+print("")
+print(f"Bank Accounts:\n {Bank.accounts}")
 print("----------------------------------------------------------------------")
+
+# Test the close_account function...arbitrarily pick a subset of accounts to mark closed
+account_numbers = [x for x in Bank.accounts.keys() if str(x) >= "8000000"] # list comprehension
+for account_number in account_numbers:
+    Bank.close_account(account_number)
+    print(f"Account: {account_number} has been closed.")
+
+# Print an updated list of accounts...should contain some accounts with status = closed
+print("")
+print(f"Bank Accounts:\n {Bank.accounts}")
