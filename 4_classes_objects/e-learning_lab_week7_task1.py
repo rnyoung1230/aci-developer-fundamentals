7c120470cc30b2b2211f3cd95e52bebca6a29ac6
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-20 18:16:04 -0600

    various updates

--- 4_classes_objects/e-learning_lab_week7_task1.py
+++ 4_classes_objects/e-learning_lab_week7_task1.py
@@ -10,8 +10,8 @@ class Artist:
         self.albums = []
 
     def __str__(self):
-        return f"\nArtist Name: {self.name}\n" \
-               f"Albums:\n{self.albums}\n"
+        return f"Artist Name: {self.name}\n" \
+               f"\nAlbums:\n{'\n'.join(str(album) for album in self.albums)}"
 
     def make_album(self, album_name=None, album_tracks=None):
         album = Album(self, album_name, album_tracks)
@@ -37,10 +37,9 @@ class Album:
         self.runtime = runtime
 
     def __str__(self):
-        return f"\nAlbum Name: {self.name}\n" \
-               f"Artist: {self.artist.name}\n" \
+        return f"Album Name: {self.name}\n" \
                f"Total Runtime: {self.runtime} minutes\n" \
-               f"\nTracks:\n{'\n'.join(str(track) for track in self.tracks)}"
+               f"\nTracks:\n{'\n'.join(str(track) for track in self.tracks)}\n"
 
 class Track:
     # Instance variables and methods
@@ -64,14 +63,13 @@ track_9 = new_artist.record_track("Purple Rain", 9, 521)
 
 tracks_a1.append(track_1)
 tracks_a1.append(track_9)
+print(f"TESTING TRACK CREATION 1\n{'\n'.join(str(track) for track in tracks_a1)}")
+print(f"-------------------------------------------------------------\n")
 
 album_1 = new_artist.make_album("Purple Rain", tracks_a1)
+print(f"TESTING ALBUM CREATION 1\n{album_1}")
+print(f"-------------------------------------------------------------\n")
 
-print("")
-print("TESTING ALBUM CREATION", album_1)
-print(f"-------------------------------------------------------------\n")
-print(f"TESTING TRACK CREATION\n{'\n'.join(str(track) for track in tracks_a1)}")
-print(f"-------------------------------------------------------------\n")
 
 tracks_a2 = []
 track_3 = new_artist.record_track("Diamonds and Pearls", 3, 285)
@@ -79,13 +77,14 @@ track_4 = new_artist.record_track("Cream", 4, 253)
 
 tracks_a2.append(track_3)
 tracks_a2.append(track_4)
+print(f"TESTING TRACK CREATION 2\n{'\n'.join(str(track) for track in tracks_a2)}")
+print(f"-------------------------------------------------------------\n")
 
 album_2 = new_artist.make_album("Diamonds and Pearls", tracks_a2)
-
-print("TESTING ALBUM CREATION", album_2)
-print(f"-------------------------------------------------------------\n")
-print(f"TESTING TRACK CREATION\n{'\n'.join(str(track) for track in tracks_a2)}")
+print(f"TESTING ALBUM CREATION 2\n{album_2}")
 print(f"-------------------------------------------------------------\n")
 
-print(f"TESTING ARTIST SUMMARY\nArtist: {new_artist.name}"
-      f"\nAlbums:\n{'\n'.join(f'{album.name} (Runtime: {album.runtime} minutes)' for album in new_artist.albums)}")
+print(f"TESTING ARTIST SUMMARY - SHORT\nArtist: {new_artist.name}"
+      f"\nAlbums:\n{'\n'.join(f'{album.name} (Runtime: {album.runtime} minutes)' for album in new_artist.albums)}\n")
+print(f"-------------------------------------------------------------\n")
+print(f"TESTING ARTIST SUMMARY - LONG\n{new_artist}")
