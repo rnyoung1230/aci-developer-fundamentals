055c4c941ab306a3d4da61f3936689b9138be01f
rnyoung1230 <7281067+rnyoung1230@users.noreply.github.com>
2025-02-23 07:04:29 -0600

    various updates

--- 4_classes_objects/class_session_labology_0220.py
+++ 4_classes_objects/class_session_labology_0220.py
@@ -53,6 +53,9 @@ class Artist:
         return f"Artist: {self.name}\n" \
                f"\nAlbums:\n{album_display}"
 
+    def __repr__(self):
+        return f"Object representation: {self.__class__.__name__}(artist_name={self.name!r})"
+
     def make_album(self, album_title):
         album = Album(album_title, self)
         self.albums.append(album)
@@ -68,6 +71,7 @@ class Album:
 
     def add_track(self, track_name, track_number, length):
         track = Track(track_name, track_number, length)
+        print(repr(track)) # for debugging
         self.tracks.append(track)
         self.runtime += track.length
 
@@ -83,6 +87,9 @@ class Album:
                f"Artist: {self.artist.name}\n" \
                f"\nTracks:\n{track_display}"
 
+    def __repr__(self):
+        return f"Object representation: {self.__class__.__name__}(album_title={self.title!r}, artist=Artist({self.artist.name!r}))"
+
 class Track:
     def __init__(self, track_name, track_number, length):
         self.number = track_number
@@ -91,12 +98,17 @@ class Track:
 
     def __str__(self):
         return f"Track Number: {self.number} " \
-               f"Name: {self.name} " \
+               f"Name: {self.name!r} " \
                f"Length: {self.length} seconds"
+
+    def __repr__(self):
+        return (f'Object representation: {self.__class__.__name__}'
+                f'(track_name={self.name!r}, track_number={self.number}, length={self.length})')
 
 ################################# TEST CODE ###########################################
 
 artist_1 = Artist("Prince")
+# print(repr(artist_1))
 # print(artist_1)
 # print("-------------------------------------------------\n")
 
@@ -106,6 +118,8 @@ album_1 = artist_1.make_album("Purple Rain")
 
 album_1.add_track("Let's Go Crazy", 1, 280)
 album_1.add_track("Purple Rain", 9, 521)
+print("")
+print(repr(album_1))
 print(album_1)
 print("-------------------------------------------------\n")
 
@@ -113,7 +127,10 @@ album_2 = artist_1.make_album("Diamonds and Pearls")
 
 album_2.add_track("Diamonds and Pearls", 3, 285)
 album_2.add_track("Cream", 4, 253)
+print("")
+print(repr(album_2))
 print(album_2)
 print("-------------------------------------------------\n")
 
+print(repr(artist_1))
 print(artist_1)
